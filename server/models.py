c759eaedd5eab1a86e560edf901f96b3cde71759
hnkisadiq <hnkisadiq@gmail.com>
2017-11-01 20:21:39 +0530

    Update models.py

--- server/models.py
+++ server/models.py
@@ -7,14 +7,20 @@ from django.contrib.gis.db import models
 class Identity(models.Model):
     adhaar_id=models.IntegerField()
     name= models.CharField(max_length=100, blank=True, default='')
+    def __str__(self):
+        return self.name
 
 class Property_house(models.Model):
     a_id=models.ForeignKey(Identity,on_delete=models.CASCADE)
     house_id=models.AutoField(primary_key=True)
+    def __str__(self):
+        return self.house_id
 
 class Property_farm(models.Model):
     a_id=models.ForeignKey(Identity,on_delete=models.CASCADE)
     farm_id=models.AutoField(primary_key=True)
+    def __str__(self):
+        return self.farm_id
 
 class House(models.Model):
     h_id=models.ForeignKey(Property_house,on_delete=models.CASCADE)
@@ -22,19 +28,20 @@ class House(models.Model):
     area=models.IntegerField()
     income=models.IntegerField()
     img = models.CharField(max_length=500,default='')
+    def __str__(self):
+        return self.h_id
 
 class Farm(models.Model):
     f_id=models.ForeignKey(Property_farm,on_delete=models.CASCADE)
     area=models.IntegerField()
     poly = models.PolygonField(default='')
+    id=models.AutoField(primary_key=True)
+    def __str__(self):
+        return self.f_id
 
 class Crop(models.Model):
-    c_id=models.ForeignKey(Farm,on_delete=models.CASCADE)
+    c_id=models.AutoField(primary_key=True)
     name = models.CharField(max_length=100,default='')
     output=models.IntegerField()
-
-class Well(models.Model):
-    w_id=models.AutoField(primary_key=True)
-    w_point = models.PointField(default='')
-    depth=models.IntegerField()
+    farm_id=models.ForeignKey(Farm,on_delete=models.CASCADE)
 
